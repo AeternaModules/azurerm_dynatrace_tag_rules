eb19fcdfc9afdc4de5fadaffc46eeb744b99c6d1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 14:49:27 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "dynatrace_tag_ruless" {
+  description = "All dynatrace_tag_rules resources"
+  value       = azurerm_dynatrace_tag_rules.dynatrace_tag_ruless
+}
+
---
+++ variables.tf
@@ -0,0 +1,47 @@
+variable "dynatrace_tag_ruless" {
+  description = <<EOT
+Map of dynatrace_tag_ruless, attributes below
+Required:
+    - monitor_id
+    - name
+Optional:
+    - log_rule (block):
+        - filtering_tag (optional, block):
+            - action (required)
+            - name (required)
+            - value (required)
+        - send_activity_logs_enabled (optional)
+        - send_azure_active_directory_logs_enabled (optional)
+        - send_subscription_logs_enabled (optional)
+    - metric_rule (block):
+        - filtering_tag (optional, block):
+            - action (required)
+            - name (required)
+            - value (required)
+        - sending_metrics_enabled (optional)
+EOT
+
+  type = map(object({
+    monitor_id = string
+    name       = string
+    log_rule = optional(object({
+      filtering_tag = optional(object({
+        action = string
+        name   = string
+        value  = string
+      }))
+      send_activity_logs_enabled               = optional(bool, false)
+      send_azure_active_directory_logs_enabled = optional(bool, false)
+      send_subscription_logs_enabled           = optional(bool, false)
+    }))
+    metric_rule = optional(object({
+      filtering_tag = optional(object({
+        action = string
+        name   = string
+        value  = string
+      }))
+      sending_metrics_enabled = optional(bool, false)
+    }))
+  }))
+}
+
