7f3d97b32b119bef0d6274a1549a9c5bd9eb2750
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:22 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "dynatrace_tag_ruleses" {
-  description = "All dynatrace_tag_rules resources"
-  value       = azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses
-}
 output "dynatrace_tag_ruleses_log_rule" {
-  description = "List of log_rule values across all dynatrace_tag_ruleses"
-  value       = [for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : v.log_rule]
+  description = "Map of log_rule values across all dynatrace_tag_ruleses, keyed the same as var.dynatrace_tag_ruleses"
+  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : k => v.log_rule }
 }
 output "dynatrace_tag_ruleses_metric_rule" {
-  description = "List of metric_rule values across all dynatrace_tag_ruleses"
-  value       = [for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : v.metric_rule]
+  description = "Map of metric_rule values across all dynatrace_tag_ruleses, keyed the same as var.dynatrace_tag_ruleses"
+  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : k => v.metric_rule }
 }
 output "dynatrace_tag_ruleses_monitor_id" {
-  description = "List of monitor_id values across all dynatrace_tag_ruleses"
-  value       = [for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : v.monitor_id]
+  description = "Map of monitor_id values across all dynatrace_tag_ruleses, keyed the same as var.dynatrace_tag_ruleses"
+  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : k => v.monitor_id }
 }
 output "dynatrace_tag_ruleses_name" {
-  description = "List of name values across all dynatrace_tag_ruleses"
-  value       = [for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : v.name]
+  description = "Map of name values across all dynatrace_tag_ruleses, keyed the same as var.dynatrace_tag_ruleses"
+  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_ruleses : k => v.name }
 }
 
