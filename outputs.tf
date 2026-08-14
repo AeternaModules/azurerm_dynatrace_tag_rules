c36c9dd64d2a2cecc583b0d04396e894529fb8b2
Daniel Allen <dan@aeternagroup.net>
2026-08-15 00:52:12 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -4,11 +4,11 @@ output "dynatrace_tag_rules_id" {
 }
 output "dynatrace_tag_rules_log_rule" {
   description = "Map of log_rule values across all dynatrace_tag_rules, keyed the same as var.dynatrace_tag_rules"
-  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_rules : k => v.log_rule if v.log_rule != null && length(v.log_rule) > 0 }
+  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_rules : k => one(v.log_rule) if v.log_rule != null && length(v.log_rule) > 0 }
 }
 output "dynatrace_tag_rules_metric_rule" {
   description = "Map of metric_rule values across all dynatrace_tag_rules, keyed the same as var.dynatrace_tag_rules"
-  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_rules : k => v.metric_rule if v.metric_rule != null && length(v.metric_rule) > 0 }
+  value       = { for k, v in azurerm_dynatrace_tag_rules.dynatrace_tag_rules : k => one(v.metric_rule) if v.metric_rule != null && length(v.metric_rule) > 0 }
 }
 output "dynatrace_tag_rules_monitor_id" {
   description = "Map of monitor_id values across all dynatrace_tag_rules, keyed the same as var.dynatrace_tag_rules"
